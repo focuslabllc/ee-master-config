30971a5d36131791515e6072d130887e1c6a74e1
Eric Lamb <eric@mithra62.com>
2012-07-15 12:07:04 -0600

    Added mechanism to use multiple databases Example usage is in config.dev.php
    Instructions:
    you use a multidimensional array for database credentials. Master uses "expressionengine" for the key and all others follow
    Backwards compatibility with single databases is maintained.
    
    Note: You MUST include all database config keys for additional databases (included in config.dev.php)

--- config/config.dev.php
+++ config/config.dev.php
@@ -10,11 +10,37 @@
  * @author     Focus Lab, LLC <dev@focuslabllc.com>
  */
 
+//single database usage
 $env_db['hostname'] = 'localhost';
 $env_db['username'] = '';
 $env_db['password'] = '';
 $env_db['database'] = '';
 
+
+$env_config['webmaster_email'] = 'dev@domain.com';
+
+/**
+Example of multiple database usage
+$env_db['expressionengine']['hostname'] = '';
+$env_db['expressionengine']['username'] = '';
+$env_db['expressionengine']['password'] = '';
+$env_db['expressionengine']['database'] = '';
+
+$env_db['another_db']['hostname'] = '';
+$env_db['another_db']['username'] = '';
+$env_db['another_db']['password'] = '';
+$env_db['another_db']['database'] = '';
+$env_db['another_db']['dbprefix'] = '';
+$env_db['another_db']['swap_pre'] = '';
+$env_db['another_db']['dbdriver'] = 'mysql';
+$env_db['another_db']['pconnect'] = FALSE;
+$env_db['another_db']['db_debug'] = TRUE;
+$env_db['another_db']['cache_on'] = FALSE;
+$env_db['another_db']['autoinit'] = FALSE;
+$env_db['another_db']['char_set'] = 'utf8';
+$env_db['another_db']['dbcollat'] = 'utf8_general_ci';
+$env_db['another_db']['cachedir'] = '';
+*/
 
 $env_config['webmaster_email'] = 'dev@domain.com';
 
